91a37802d7d5c7d53f39ddf19d286a3b03a2b99d
Stalloyde <stalloyde93@gmail.com>
2024-03-30 10:46:47 +0800

    Add labels

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,41 @@ import Button from './components/Button.vue'
 <template>
   <div>
     <Button />
+    Default Button
   </div>
   <div>
     <Button type="primary" content="leftIconText" />
+    Primary Button, Left Icon & Text
   </div>
   <div>
     <Button type="primary" content="rightIconText" />
+    Primary Button, Right Icon & Text
   </div>
   <div>
     <Button type="primary" content="text" />
+    Primary Button, Text
   </div>
   <div>
     <Button type="primary" content="icon" />
+    Primary Button, Icon
   </div>
 
   <br />
 
   <div>
     <Button type="secondary" content="leftIconText" />
+    Secondary Button, Left Icon & Text
   </div>
   <div>
     <Button type="secondary" content="rightIconText" />
+    Secondary Button, Right Icon & Text
   </div>
   <div>
     <Button type="secondary" content="text" />
+    Secondary Button, Text
   </div>
   <div>
     <Button type="secondary" content="icon" />
+    Secondary Button, Icon
   </div>
 </template>
